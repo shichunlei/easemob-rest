ddb1fb14a0afdc8c83d60468a50ffdd79587bb01
Azazle <cronnorc@gmail.com>
2014-08-11 00:45:21 +0800

    add bind admin interface

--- lib/easemob.rb
+++ lib/easemob.rb
@@ -27,34 +27,37 @@ module IM
     end
 
     # 授权注册模式-创建帐号
-    def create(email)
-      usr_name = gen_esmb_usr(email)
+    def create(email, passwd)
+      usr_name = gen_usr_name(email)
       authorization = "Bearer " + @access_token
       header = { "Authorization" => authorization }
 
       url = Constants::EASEMOB_BASE_URL + '/users'
       params = {
         username: usr_name,
-        password: '123456'  # 暂用账号的作为密码
+        password: passwd
       }
       uri, req = @http_client.post_request(url, params, header)
       res = @http_client.submit(uri, req)
-      puts JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess)
+      JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess)
     end
 
     # 删除已有账号
     def delete(email)
-      usr_name = gen_esmb_usr(email)
+      usr_name = gen_usr_name(email)
       authorization = "Bearer " + @access_token
       header = { "Authorization" => authorization }
       url = Constants::EASEMOB_BASE_URL + '/users/' + usr_name
       uri, req = @http_client.del_request(url, header)
       res = @http_client.submit(uri, req)
-      puts JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess)
+      JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess)
     end
 
     # 管理员发送消息给用户或者群组
     def send_message(from, msg, to_type, to_list, ext)
+      authorization = "Bearer " + @access_token
+      header = { "Authorization" => authorization }
+
       url = Constants::EASEMOB_BASE_URL + '/messages'
       params = {
         target_type: to_type,
@@ -63,25 +66,40 @@ module IM
         from: from,
         ext: ext
       }
-      uri, req = @http_client.post_request(url, params)
+      uri, req = @http_client.post_request(url, params, header)
       res = @http_client.submit(uri, req)
-      puts JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess)
+      JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess)
+    end
+
+    # 将用户绑定为管理员
+    def bind_admin(usr_name)
+      authorization = "Bearer " + @access_token
+      header = { "Authorization" => authorization }
+
+      url = Constants::EASEMOB_BASE_URL + '/users/' + usr_name + '/roles/admin'
+      uri, req = @http_client.post_request(url, {}, header)
+      res = @http_client.submit(uri, req)
+      JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess)
     end
 
     private
       # 根据邮箱生成环信账号
-      def gen_esmb_usr(email)
+      def gen_usr_name(email)
         Digest::MD5.hexdigest(email)
       end    
   end
 end
 
-# Test method
-easemob = IM::Easemob.new
+# easemob = IM::Easemob.new
+
 # Login and authorize
-easemob.authorize
+# easemob.authorize
+
 # Create user
-easemob.create('cronnorc@gmail.com')
+# easemob.create('cronnorc@gmail.com', 'udesk')
+
 # Delete user
 # easemob.delete('cronnorc@gmail.com')
 
+# Bind to admin
+# curl -X POST -H 'Authorization: Bearer {token}' -H 'Content-Type: application/json' 'https://a1.easemob.com/{orgName}/{appName}/users/{username}/roles/admin'
